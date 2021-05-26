78054062103d3eee720f58bf270da1409c2a5405
Tanu Goyal <tanu.b.goyal@accenture.com>
2021-05-26 21:22:05 +0530

    changed design

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -9,18 +9,16 @@ class Home extends Component {
                 <section className="screen-center-content">
                     {data[2].intro}
                 </section>
-            </div>
-
-            <section className="work-section">
-            <div className="line-hr">
+                <div className="line-hr">
                 <span>
                    <h1>Some of my Works!</h1> 
               </span>
-            </div>
-                <ul  className="project-section ">
-                    {data[2].githubprojects.map((project) => <li><a href={project.githublink} target="_blank" rel="noreferrer">{project.projectName}</a></li>)}
+              </div>
+              <ul  className="project-section ">
+                    {data[2].githubprojects.map((project) => <li><a href={project.githublink} className="projectsLink" target="_blank" rel="noreferrer">{project.projectName}</a></li>)}
                 </ul>
-            </section>
+            </div>
+           
         </>
     }
 }
