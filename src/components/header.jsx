347d30aeaf876339c9797b51f6c54ba3aeeb12f8
Tanu Goyal <tanu.b.goyal@accenture.com>
2021-06-04 15:49:48 +0530

    brand img

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import tanuImg from '../assets/tanugoyal.jpg'
 import '../styles/styles.css'
 
 export default class Header extends Component {
     render() {
         return<div className="navigation-container">
-            <div className="nav">
-                <Link to="/" className="brand">NEW CODER</Link>
+                <div className="nav">
+                    <Link to="/">
+                        <img src={tanuImg} alt="logo" className="brand"/>
+                    </Link>
                 <div>
                 <Link to="/" className="nav-content">Home</Link>
                 <Link to="/projects" className="nav-content">Projects</Link>
